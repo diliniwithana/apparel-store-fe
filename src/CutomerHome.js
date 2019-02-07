bb506e565d66de9b3fff0b74045992ad805123ee
chamaral <chamara.liyanage@syscolabs.com>
2019-02-07 09:24:04 +0530

    implement customer and retailer home

--- src/CutomerHome.js
+++ src/CutomerHome.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
-
-import passwordHash from "password-hash";
-import emailValidator from "email-validator";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
 export default class CustomerHome extends Component {
   constructor(props) {
@@ -13,140 +18,110 @@ export default class CustomerHome extends Component {
       password: "",
       errorMessage: ""
     };
-
-    this.hash = "";
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
-  handleEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
-
-  handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    if (this.validateForm()) {
-      let { email, password } = this.state;
-      if (emailValidator.validate(email)) {
-        //fetch
-        fetch(`http://localhost:3001/users/login/${email}`)
-          .then(response => response.json())
-          .then(userObject => {
-            if (userObject.password && userObject.role) {
-              let isPasswordMatch = passwordHash.verify(
-                password,
-                userObject.password
-              );
-              if (isPasswordMatch) {
-                console.log(
-                  "Success " + userObject.email + " | " + userObject.role
-                );
-              } else {
-                //invalid password
-                console.log("Wrong Pass");
-                this.setState({ errorMessage: "Wrong password" });
-              }
-            } else {
-              //invalid userName
-              console.log("Wrong Email");
-              this.setState({ errorMessage: "Wrong user name" });
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          });
-
-        this.setState({ errorMessage: "" });
-      } else {
-        this.setState({ errorMessage: "Enter a valid email" });
-      }
-    } else {
-      this.setState({ errorMessage: "Fields are empty" });
-    }
-  };
 
   render() {
     return (
-        <React.Fragment>
-          <section className="login-block">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4 login-sec">
-                  <h2 className="text-center">Customer Home</h2>
-                  <form className="login-form">
-                    <div className="form-group">
-                      <label
-                        htmlFor="exampleInputEmail1"
-                        className="text-uppercase"
-                      >
-                        Email
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder=""
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label
-                        htmlFor="exampleInputPassword1"
-                        className="text-uppercase"
-                      >
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        placeholder=""
-                        onChange={this.handlePasswordChange}
-                      />
-                    </div>
-                    <div
-                      className="alert alert-danger alert-dismissible"
-                      style={
-                        this.state.errorMessage
-                          ? { display: "block" }
-                          : { display: "none" }
-                      }
-                    >
-                      <strong>OOPS!</strong> {this.state.errorMessage}
-                    </div>
-                    <div className="form-check">
-                      <input type="checkbox" className="form-check-input" />
-                      <small>Remember Me</small>
-                      <button
-                        type="submit"
-                        className="btn btn-login float-right"
-                        onClick={this.handleSubmit}
-                      >
-                        Login
-                      </button>
-                    </div>
-                    <div className="form-group text-center register">
-                      <Link to="/register">
-                        <h2 className="text-center">Register</h2>
-                      </Link>
-                    </div>
-                  </form>
-                </div>
-                <div className="col-md-8 login-sec">
-                  <img src="cloths.jpeg" alt="Chania" />
-                </div>
+      <React.Fragment>
+        <header>
+          <Navbar color="dark" light expand="md">
+            <NavbarBrand id="appName" href="/">
+              FitsMe
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <img id="profile-pic" src="user.png" alt="profile-pic" />
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    id="profile-name"
+                    href="https://github.com/reactstrap/reactstrap"
+                  >
+                    Dilini Withana
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </header>
+        <br />
+
+        <div class="container">
+          <div class="card-deck mb-3 text-center">
+            <div class="card mb-4 box-shadow">
+              <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Free</h4>
+              </div>
+              <div class="card-body">
+                <img
+                  src="shirt1.jpeg"
+                  alt="shirt"
+                  data-holder-rendered="true"
+                  style={{ width: "200px", height: "250px" }}
+                />
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li>10 users included</li>
+                  <li>2 GB of storage</li>
+                  <li>Email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button
+                  type="button"
+                  class="btn btn-lg btn-block btn-outline-primary"
+                >
+                  Sign up for free
+                </button>
               </div>
             </div>
-          </section>
-        </React.Fragment>
+            <div class="card mb-4 box-shadow">
+              <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Pro</h4>
+              </div>
+              <div class="card-body">
+                <img
+                  src="shirt2.jpeg"
+                  alt="shirt"
+                  data-holder-rendered="true"
+                  style={{ width: "200px", height: "250px" }}
+                />
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li>20 users included</li>
+                  <li>10 GB of storage</li>
+                  <li>Priority email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-primary">
+                  Get started
+                </button>
+              </div>
+            </div>
+            <div class="card mb-4 box-shadow">
+              <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Enterprise</h4>
+              </div>
+              <div class="card-body">
+                <img
+                  src="shirt3.jpeg"
+                  alt="shirt"
+                  data-holder-rendered="true"
+                  style={{ width: "200px", height: "250px" }}
+                />
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li>30 users included</li>
+                  <li>15 GB of storage</li>
+                  <li>Phone and email support</li>
+                  <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-primary">
+                  Contact us
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/RetailerHome.js
+++ src/RetailerHome.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
-import passwordHash from "password-hash";
-import emailValidator from "email-validator";
-
-export default class CustomerHome extends Component {
+export default class RetailerHome extends Component {
   constructor(props) {
     super(props);
 
@@ -14,139 +19,98 @@ export default class CustomerHome extends Component {
       errorMessage: ""
     };
 
-    this.hash = "";
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
-  handleEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
-
-  handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    if (this.validateForm()) {
-      let { email, password } = this.state;
-      if (emailValidator.validate(email)) {
-        //fetch
-        fetch(`http://localhost:3001/users/login/${email}`)
-          .then(response => response.json())
-          .then(userObject => {
-            if (userObject.password && userObject.role) {
-              let isPasswordMatch = passwordHash.verify(
-                password,
-                userObject.password
-              );
-              if (isPasswordMatch) {
-                console.log(
-                  "Success " + userObject.email + " | " + userObject.role
-                );
-              } else {
-                //invalid password
-                console.log("Wrong Pass");
-                this.setState({ errorMessage: "Wrong password" });
-              }
-            } else {
-              //invalid userName
-              console.log("Wrong Email");
-              this.setState({ errorMessage: "Wrong user name" });
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          });
-
-        this.setState({ errorMessage: "" });
-      } else {
-        this.setState({ errorMessage: "Enter a valid email" });
-      }
-    } else {
-      this.setState({ errorMessage: "Fields are empty" });
-    }
-  };
 
   render() {
     return (
-        <React.Fragment>
-          <section className="login-block">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4 login-sec">
-                  <h2 className="text-center">Retailer Home</h2>
-                  <form className="login-form">
-                    <div className="form-group">
-                      <label
-                        htmlFor="exampleInputEmail1"
-                        className="text-uppercase"
-                      >
-                        Email
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder=""
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label
-                        htmlFor="exampleInputPassword1"
-                        className="text-uppercase"
-                      >
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        placeholder=""
-                        onChange={this.handlePasswordChange}
-                      />
-                    </div>
-                    <div
-                      className="alert alert-danger alert-dismissible"
-                      style={
-                        this.state.errorMessage
-                          ? { display: "block" }
-                          : { display: "none" }
-                      }
-                    >
-                      <strong>OOPS!</strong> {this.state.errorMessage}
-                    </div>
-                    <div className="form-check">
-                      <input type="checkbox" className="form-check-input" />
-                      <small>Remember Me</small>
-                      <button
-                        type="submit"
-                        className="btn btn-login float-right"
-                        onClick={this.handleSubmit}
-                      >
-                        Login
-                      </button>
-                    </div>
-                    <div className="form-group text-center register">
-                      <Link to="/register">
-                        <h2 className="text-center">Register</h2>
-                      </Link>
-                    </div>
-                  </form>
-                </div>
-                <div className="col-md-8 login-sec">
-                  <img src="cloths.jpeg" alt="Chania" />
-                </div>
+      <React.Fragment>
+        <header>
+          <Navbar color="dark" light expand="md">
+            <NavbarBrand id="appName" href="/">
+              FitsMe
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <img id="profile-pic" src="user.png" alt="profile-pic" />
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    id="profile-name"
+                    href="https://github.com/reactstrap/reactstrap"
+                  >
+                    Rich Look
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </header>
+        <br></br>
+        <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+          <div className="col-md-6 px-0">
+            <h1 className="display-4 font-italic">Welcome Rich Look</h1>
+            <p className="lead my-3">
+              Online 2D comparision model is the light weight solution to
+              current online market place.
+            </p>
+          </div>
+        </div>
+
+        <div className="row mb-2">
+          <div className="col-md-6">
+            <div className="card flex-md-row mb-4 box-shadow h-md-250">
+              <div className="card-body d-flex flex-column align-items-start">
+                <strong className="d-inline-block mb-2 text-primary">Add</strong>
+                <h3 className="mb-0">
+                  <a className="text-dark" href="#">
+                    Add Garment Measurement
+                  </a>
+                </h3>
+                <br />
+                <p className="card-text mb-auto">
+                  Garment can be added to the system with its finished garment measurements.
+                </p>
+                <a href="#">Add Garment</a>
               </div>
+              <img
+                className="card-img-right flex-auto d-none d-lg-block"
+                data-src="holder.js/200x250?theme=thumb"
+                alt="Thumbnail [200x250]"
+                style={{ width: "200px", height: "250px" }}
+                src="cloth1.jpeg"
+                data-holder-rendered="true"
+              />
             </div>
-          </section>
-        </React.Fragment>
+          </div>
+          <div className="col-md-6">
+            <div className="card flex-md-row mb-4 box-shadow h-md-250">
+              <div className="card-body d-flex flex-column align-items-start">
+                <strong className="d-inline-block mb-2 text-success">Edit</strong>
+                <h3 className="mb-0">
+                  <a className="text-dark" href="#">
+                    Edit Garment Measurement
+                  </a>
+                </h3>
+                <br />{" "}
+                <p className="card-text mb-auto">
+                  Garment details can be updated using with new pictures and details
+                </p>
+                <a href="#">Edit Garment</a>
+              </div>
+              <img
+                className="card-img-right flex-auto d-none d-lg-block"
+                data-src="holder.js/200x250?theme=thumb"
+                alt="Thumbnail [200x250]"
+                src="cloths.jpeg"
+                data-holder-rendered="true"
+                style={{ width: "200px", height: "250px" }}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
